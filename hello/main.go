041a93db16aa2253947f4c69f88616a57cd38774
Alex Bykov <spbeat@mail.ru>
2017-04-25 22:36:16 +0300

    callbacks

--- hello/main.go
+++ hello/main.go
@@ -3,7 +3,10 @@ package main
 import "fmt"
 
 var first string = "Hello"
-const ex rune = 2305888
+
+const ex rune = 255455432
+
+var nums = []int16{2, 3, 4, 5, 6, 7, 8}
 
 func main() {
 	second := "tiny hare"
@@ -14,6 +17,9 @@ func main() {
 	fmt.Println(closure(10)(4))
 	looper()
 	Breaker()
+	callbacker(nums, func(n int16) {
+		fmt.Println(n)
+	})
 }
 
 func printer(a string, b string) {
@@ -29,5 +35,11 @@ func looper() {
 func closure(number int) func(add int) int {
 	return func(add int) int {
 		return number + add
+	}
+}
+
+func callbacker(numbers []int16, cb func(int16)) {
+	for _, i := range numbers {
+		cb(i)
 	}
 }
